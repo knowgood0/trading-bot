8b14b29600c07b68d4fbdbf81a8254bd5c3555b7
knowgood0 <knowgooddesign@gmail.com>
2026-08-03 18:44:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, jsonify
 from webull_client import (
     test_webull_connection,
     test_options,
-    paper_buy_spy
+    paper_buy_spy,
+    debug_place_option_request
 )
 
 
@@ -12,6 +13,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+
     return jsonify({
         "status": "Trading bot running"
     })
@@ -19,6 +21,7 @@ def home():
 
 @app.route("/webull-test")
 def webull_test():
+
     return jsonify(
         test_webull_connection()
     )
@@ -26,6 +29,7 @@ def webull_test():
 
 @app.route("/options-test")
 def options_test():
+
     return jsonify(
         test_options()
     )
@@ -33,12 +37,22 @@ def options_test():
 
 @app.route("/buy-test")
 def buy_test():
+
     return jsonify(
         paper_buy_spy()
     )
 
 
+@app.route("/debug-option-order")
+def debug_option_order():
+
+    return jsonify(
+        debug_place_option_request()
+    )
+
+
 if __name__ == "__main__":
+
     app.run(
         host="0.0.0.0",
         port=10000
